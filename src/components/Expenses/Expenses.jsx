d51c48923056dd9cbec405dbe601d5cac414d40a
illusion <17003685+fredkydd@users.noreply.github.com>
2023-10-05 02:09:29 +0400

    added derived || computed state

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -7,10 +7,27 @@ import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpenseFilter';
 
 export default function Expenses({ expenses }) {
+  // *i use filteredText variable because useState will render this component again when i click the option value to change year.No need to another state!
   const [selectedYear, setSelectedYear] = useState(null),
     selectedYearPropsHandler = (year) => {
       setSelectedYear(year);
+      console.log(`type of selectedYear is \n ${typeof selectedYear}`);
     };
+  let filteredText = '2020, 2021, 2022, 2023',
+    hiddenVisible = 'hidden';
+
+  if (selectedYear === 2023) {
+    filteredText = '2020, 2021, 2022';
+  } else if (selectedYear === 2022) {
+    filteredText = '2020, 2021, 2023';
+  } else if (selectedYear === 2021) {
+    filteredText = '2020, 2022, 2023';
+  } else if (selectedYear === 2020) {
+    filteredText = ' 2021, 2022, 2023';
+  } else {
+    filteredText = '2020, 2021, 2022, 2023';
+    hiddenVisible = 'visible';
+  }
 
   const filteredExpenses = expenses.filter((expense) => {
       const expenseDate = new Date(expense.date); // *Convert to Date object
@@ -38,7 +55,13 @@ export default function Expenses({ expenses }) {
   return (
     <Card className={styles['expenses']}>
       <ExpensesFilter selectedYearProps={selectedYearPropsHandler} />
-
+      <p className={styles.filteredText}>
+        Data for years:{' '}
+        <strong>
+          {' '}
+          {filteredText} are {hiddenVisible}.
+        </strong>
+      </p>
       {expenseItems.length === 0 ? (
         <strong className={styles.error}>
           No expenses found for the {selectedYear} year
